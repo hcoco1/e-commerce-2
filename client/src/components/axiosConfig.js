9a5a039fc11ed7e382a93eade69ad62f5f0a0263
Ivan <arias.ivan@gmail.com>
2023-10-05 17:25:28 -0400

    axios config  updated

--- client/src/components/axiosConfig.js
+++ client/src/components/axiosConfig.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://phase5-app-tyia.onrender.com' : 'http://localhost:5555';
+
 const instance = axios.create({
-    baseURL: 'http://localhost:5555', 
-    timeout: 10000, 
+    baseURL,
+    timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
     }
 });
 
 export default instance;
+
 
