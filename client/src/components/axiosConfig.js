849531d61cc301ff62ecf55c612b8a0cc8b9e288
Ivan <arias.ivan@gmail.com>
2023-10-06 08:54:15 -0400

    testing PostgreSQL

--- client/src/components/axiosConfig.js
+++ client/src/components/axiosConfig.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://phase5-app-tyia.onrender.com' : 'http://localhost:5555';
+
 
 const instance = axios.create({
-    baseURL,
-    timeout: 10000,
+    
+   
     headers: {
         'Content-Type': 'application/json'
     }
--- server/config.py
+++ server/config.py
@@ -29,18 +29,7 @@ db.init_app(app)
 bcrypt = Bcrypt(app)
 
 
-from flask_cors import CORS
-
-# Determine the allowed origins based on the environment
-if app.config['ENV'] == 'development':
-    # Allow requests from your development frontend (http://localhost:3000)
-    allowed_origins = ['http://localhost:3000']
-else:
-    # Configure allowed origins for your production domain(s)
-    allowed_origins = ['https://phase5-app-tyia.onrender.com']
-
-# Configure CORS with the determined allowed origins
-CORS(app, resources={r"/*": {"origins": allowed_origins}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
 
 
 
