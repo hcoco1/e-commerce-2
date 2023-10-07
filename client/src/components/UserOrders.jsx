2558c528935538a83af0bdef14a52fda8d993f0d
Ivan <arias.ivan@gmail.com>
2023-10-07 18:52:49 -0400

    component updated

--- client/src/components/UserOrders.jsx
+++ client/src/components/UserOrders.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import api from './api';
 import styled from 'styled-components';
